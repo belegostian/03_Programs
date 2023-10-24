d5205681a1393f110d3f49904dbfe7ecb93adeab
Ian <r10522701@ntu.edu.tw>
2023-10-24 12:24:59 +0800

    minor

---
+++ 04_RiskAnalyze/scenario_init.py
@@ -0,0 +1,7 @@
+from random import randint, shuffle, choice, choices, sample, random, uniform
+from collections import defaultdict
+
+# 1. Device List
+# Key: device_order, Value: {'type': string,  'fac_group': int, 'net_interface': int}
+device_list = {}
+
--- 05_Containernet/01_client/ctest1.py
+++ 05_Containernet/01_client/ctest1.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from asyncua import ua, Client
 
         
-SERVER_URL = 'opc.tcp://localhost:4840'
+SERVER_URL = 'opc.tcp://10.0.0.252:4840'
 SERVER_NAME = 'Test CNC Server'
 
 _logger = logging.getLogger(__name__)
@@ -42,74 +42,80 @@ def update_plot(ax, speed_line, torque_line, time_values, speed_values, torque_v
     return time_values, speed_values, torque_values # Return updated values
         
 async def main():
-    async with Client(url=SERVER_URL) as client:
-        
-        # 指定節點ID搜尋
-        CmdSpeed = client.get_node(ua.NodeId(2019, 1))
-        CmdTorque = client.get_node(ua.NodeId(2032, 1))
-        
-        ActSpeed = client.get_node(ua.NodeId(2005, 1))
-        act_speed = copy.copy(await ActSpeed.read_value())
-        ActTorque = client.get_node(ua.NodeId(2029, 1))
-        act_torque = copy.copy(await ActTorque.read_value())
-        ActPower = client.get_node(ua.NodeId(2026, 1))
-        act_power = copy.copy(await ActPower.read_value())
-        
-        Voltage = client.get_node(ua.NodeId(2037, 1))
-        voltage = copy.copy(await Voltage.read_value())
-        Current = client.get_node(ua.NodeId(2039, 1))
-        current = copy.copy(await Current.read_value())
-        
-        # 按階層搜尋
-        # Voltage = await client.nodes.root.get_child(['0:Objects', '3:Machines', '1tCNC_spindle', '1:Voltage'])
-        # Current = await client.nodes.root.get_child(['0:Objects', '3:Machines', '1tCNC_spindle', '1:Current'])
-        
-        # Initialize the plot
-        ax, speed_line, torque_line = initialize_plot()
-        time_values, speed_values, torque_values = [], [], []
-        
-        while True:
-            start_time = time.time()
+    while True:
+        try:
+            async with Client(url=SERVER_URL) as client:
             
-            # 網路負荷
-            await CmdSpeed.write_value(8000.0) # 指定轉速
-            await CmdTorque.write_value(90.0) # 指定轉矩
-            
-            ActSpeed = client.get_node(ua.NodeId(2005, 1))
-            act_speed = copy.copy(await ActSpeed.read_value())
-            ActTorque = client.get_node(ua.NodeId(2029, 1))
-            act_torque = copy.copy(await ActTorque.read_value())
-            ActPower = client.get_node(ua.NodeId(2026, 1))
-            act_power = copy.copy(await ActPower.read_value())
-            
-            Voltage = client.get_node(ua.NodeId(2037, 1))
-            voltage = copy.copy(await Voltage.read_value())
-            Current = client.get_node(ua.NodeId(2039, 1))
-            current = copy.copy(await Current.read_value())
-            
-            await asyncio.sleep(0.5) 
-            
-            _logger.info("\n實際轉速: %d rpm, 實際轉矩: %d Nm, 實際功率: %d W", act_speed, act_torque, act_power)
-            _logger.info("電壓: %d V, 電流: %d I\n", voltage, current)
-            
-            mid_time = time.time()
-
-            # CPU負荷
-            time_values.append(start_time)
-            speed_values.append(act_speed)
-            torque_values.append(act_torque)
-            
-            time_values, speed_values, torque_values = update_plot(ax, speed_line, torque_line, time_values, speed_values, torque_values)
-            
-            size = int(100)
-            iterations = int(50)
-            for _ in range(iterations):
-                a = np.random.rand(size, size)
-                b = np.random.rand(size, size)
-                np.dot(a, b)
+                # 指定節點ID搜尋
+                CmdSpeed = client.get_node(ua.NodeId(2019, 1))
+                CmdTorque = client.get_node(ua.NodeId(2032, 1))
                 
-            _logger.info("\n總耗時: %d ms, 網路請求耗時: %d ms, 計算耗時: %d ms", (time.time() - start_time)* 1000, (mid_time - start_time)* 1000, (time.time() - mid_time)* 1000)
-            
+                ActSpeed = client.get_node(ua.NodeId(2005, 1))
+                act_speed = copy.copy(await ActSpeed.read_value())
+                ActTorque = client.get_node(ua.NodeId(2029, 1))
+                act_torque = copy.copy(await ActTorque.read_value())
+                ActPower = client.get_node(ua.NodeId(2026, 1))
+                act_power = copy.copy(await ActPower.read_value())
+                
+                Voltage = client.get_node(ua.NodeId(2037, 1))
+                voltage = copy.copy(await Voltage.read_value())
+                Current = client.get_node(ua.NodeId(2039, 1))
+                current = copy.copy(await Current.read_value())
+                
+                # 按階層搜尋
+                # Voltage = await client.nodes.root.get_child(['0:Objects', '3:Machines', '1tCNC_spindle', '1:Voltage'])
+                # Current = await client.nodes.root.get_child(['0:Objects', '3:Machines', '1tCNC_spindle', '1:Current'])
+                
+                # Initialize the plot
+                ax, speed_line, torque_line = initialize_plot()
+                time_values, speed_values, torque_values = [], [], []
+                
+                while True:
+                    start_time = time.time()
+                    
+                    # 網路負荷
+                    await CmdSpeed.write_value(8000.0) # 指定轉速
+                    await CmdTorque.write_value(90.0) # 指定轉矩
+                    
+                    ActSpeed = client.get_node(ua.NodeId(2005, 1))
+                    act_speed = copy.copy(await ActSpeed.read_value())
+                    ActTorque = client.get_node(ua.NodeId(2029, 1))
+                    act_torque = copy.copy(await ActTorque.read_value())
+                    ActPower = client.get_node(ua.NodeId(2026, 1))
+                    act_power = copy.copy(await ActPower.read_value())
+                    
+                    Voltage = client.get_node(ua.NodeId(2037, 1))
+                    voltage = copy.copy(await Voltage.read_value())
+                    Current = client.get_node(ua.NodeId(2039, 1))
+                    current = copy.copy(await Current.read_value())
+                    
+                    await asyncio.sleep(0.5) 
+                    
+                    _logger.info("\n實際轉速: %d rpm, 實際轉矩: %d Nm, 實際功率: %d W", act_speed, act_torque, act_power)
+                    _logger.info("電壓: %d V, 電流: %d I\n", voltage, current)
+                    
+                    mid_time = time.time()
+
+                    # CPU負荷
+                    time_values.append(start_time)
+                    speed_values.append(act_speed)
+                    torque_values.append(act_torque)
+                    
+                    time_values, speed_values, torque_values = update_plot(ax, speed_line, torque_line, time_values, speed_values, torque_values)
+                    
+                    size = int(100)
+                    iterations = int(50)
+                    for _ in range(iterations):
+                        a = np.random.rand(size, size)
+                        b = np.random.rand(size, size)
+                        np.dot(a, b)
+                        
+                    _logger.info("\n總耗時: %d ms, 網路請求耗時: %d ms, 計算耗時: %d ms", (time.time() - start_time)* 1000, (mid_time - start_time)* 1000, (time.time() - mid_time)* 1000)
+
+        except Exception as e:
+            _logger.exception(f"Failed to connect to {SERVER_URL}. Retrying in 10 seconds. Error: {e}")
+            await asyncio.sleep(5)
+                
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
